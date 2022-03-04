1c14bcfa51c674383445a0b63a9cd672409abb15
RodrigoProgram22 <rodrigoprograma22@gmail.com>
2022-03-04 00:00:28 -0300

    Primer boton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CarouselComponent } from './components/header/carousel/carousel.component';
+import { NavbarComponent } from './components/header/navbar/navbar.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CarouselComponent
+    CarouselComponent,
+    NavbarComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
